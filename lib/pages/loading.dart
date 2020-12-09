b6cede2f8ee108224ca78202436ff4a15d439cd2
Niyonsengaeric <niyeric11@gmail.com>
2020-12-09 07:18:37 +0200

    add world time api
    - call london api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,18 +8,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
+    // Make request
     Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
+        await get('http://worldtimeapi.org/api/timezone/Europe/London');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+
+    // print(datetime);
+    // print(ofset);
+
+    // create a date time object
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
